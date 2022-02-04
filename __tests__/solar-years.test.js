c768e8053f8f4918113dc31040f15da534f0507c
Mikah Mathews <mikah@outofband.org>
2022-02-04 11:09:12 -0800

    Refactor test to have testYear called before each test

--- __tests__/solar-years.test.js
+++ __tests__/solar-years.test.js
@@ -1,20 +1,21 @@
 import SolarYear from './../src/js/solar-years.js'
 
 describe('Solar Year', () => {
+  let testYear
+  beforeEach(() => {
+    testYear = new SolarYear(21);
+  });
 
   test('Should take inputted age', () => {
-    const testYear = new SolarYear(21);
     expect(testYear.age).toEqual(21);
   });
 
   test('Should return age in days', () => {
-    const testYear = new SolarYear(21);
     expect(testYear.ageDays()).toEqual(7665);
   });
 
   test('Should return age in Mercury Years', () => {
-    const testYear = new SolarYear(21);
-    const ageDays = testYear.ageDays();
+    let ageDays = testYear.ageDays();
     expect(testYear.mercury(ageDays)).toEqual(87);
   });
 
